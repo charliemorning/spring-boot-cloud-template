cfe7ad1dbb8b6ebeb17c2b20bda35cd0ae39a8e3
Charlie <zhangchen143@gmail.com>
2021-01-28 23:14:01 +0800

    init commit

---
+++ spring-boot-cloud-template-common/src/main/java/org/charlie/template/configure/ElasticSearchConfig.java
@@ -0,0 +1,16 @@
+package org.charlie.template.configure;
+
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component()
+@ConfigurationProperties(prefix = "es")
+public class ElasticSearchConfig {
+    private String urls;
+    public String[] getUrls() {
+        return urls.split(",");
+    }
+}
