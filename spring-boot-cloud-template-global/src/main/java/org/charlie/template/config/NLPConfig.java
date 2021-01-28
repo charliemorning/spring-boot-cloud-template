66703dd937ab1be81347706ba999860dc5d0452b
charliemorning <charliegithub@gmail.com>
2021-01-28 23:16:37 +0800

    Delete spring-boot-cloud-template-global/src/main/java/org/charlie/template directory

--- spring-boot-cloud-template-global/src/main/java/org/charlie/template/config/NLPConfig.java
+++
@@ -1,8 +0,0 @@
-package org.charlie.template.config;
-
-import lombok.Data;
-
-@Data
-public class NLPConfig {
-    private NLPInferPlatConfig nlpInferencePlatform;
-}
--- spring-boot-cloud-template-global/src/main/java/org/charlie/template/config/NLPInferPlatConfig.java
+++
@@ -1,9 +0,0 @@
-package org.charlie.template.config;
-
-import lombok.Data;
-
-@Data
-public class NLPInferPlatConfig {
-    private String ip;
-    private String port;
-}
