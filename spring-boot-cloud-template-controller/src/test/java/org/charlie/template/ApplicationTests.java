d35ce56373baa776b7adb9f0291ff67fa9ed63f9
charlie <zhangchen143@gmail.com>
2021-02-01 13:59:02 +0800

    add test file

--- spring-boot-cloud-template-controller/src/test/java/org/charlie/template/ApplicationTests.java
+++ spring-boot-cloud-template-controller/src/test/java/org/charlie/template/ApplicationTests.java
@@ -1,12 +1,21 @@
 package org.charlie.template;
 
+import org.charlie.template.controller.FooController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest
 public class ApplicationTests {
+
+    @Autowired
+    FooController fooController;
+
     @Test
     public void contextLoads() {
+        assertThat(fooController).isNotNull();
     }
 }
