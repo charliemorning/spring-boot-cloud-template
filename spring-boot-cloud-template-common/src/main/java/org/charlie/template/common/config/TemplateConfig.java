652d5be140d9c26478b6a3d0e7217cf298c1b569
charlie <zhangchen143@gmail.com>
2021-07-21 19:48:41 +0800

    add redis support

--- spring-boot-cloud-template-common/src/main/java/org/charlie/template/common/config/TemplateConfig.java
+++ spring-boot-cloud-template-common/src/main/java/org/charlie/template/common/config/TemplateConfig.java
@@ -10,4 +10,5 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "template")
 public class TemplateConfig {
     private int threadNum;
+    private String redisKeyPrefix;
 }
