2e082298e204c360028194db04672110627b0936
Charlie <zhangchen143@gmail.com>
2021-08-05 00:04:18 +0800

    bug fix

--- spring-boot-cloud-template-controller/src/test/java/org/charlie/template/common/util/http/HttpClientUtilityTest.java
+++ spring-boot-cloud-template-controller/src/test/java/org/charlie/template/common/util/http/HttpClientUtilityTest.java
@@ -6,6 +6,7 @@ import org.charlie.template.common.utility.http.HttpClientUtil;
 import org.charlie.template.common.utility.http.Method;
 import org.charlie.template.common.utility.thread.ThreadUtil;
 import org.junit.Test;
+import org.springframework.util.StopWatch;
 
 
 @Slf4j
@@ -14,19 +15,26 @@ public class HttpClientUtilityTest {
     @Test
     public void postTest() throws JsonProcessingException {
 
+        log.info("start test");
         int postCount = 100000;
+        StopWatch watch = new StopWatch();
+        watch.start();
         while (postCount-- > 0) {
-            ThreadUtil.run(new Runnable() {
+            ThreadUtil.submit(new Runnable() {
                 @Override
                 public void run() {
                     try {
-                        HttpClientUtil.request("http://www.baidu.com", Method.GET, 10000);
+                        log.info(HttpClientUtil.request("http://127.0.0.1:9000", Method.GET, 10000));
                     } catch (JsonProcessingException e) {
                         e.printStackTrace();
                     }
                 }
             });
         }
+        watch.stop();
+        log.info("end");
+        log.info(watch.prettyPrint());
+        for (;;);
 
     }
 }
