fd5a0a226d9f3734dfc7fb68dfe0d56b6ff1d5f8
Charlie <zhangchen143@gmail.com>
2021-08-18 00:31:16 +0800

    remove unused constants

--- spring-boot-cloud-template-framework/src/main/java/org/charlie/template/framework/constants/TemplateConstants.java
+++ spring-boot-cloud-template-framework/src/main/java/org/charlie/template/framework/constants/TemplateConstants.java
@@ -2,6 +2,4 @@ package org.charlie.template.framework.constants;
 
 public class TemplateConstants {
     public final static String TRACE_ID_HEADER_KEY = "global_trace_id";
-
-    public final static int CONNECTION_CLOSE_INTERVAL = 1;
 }
