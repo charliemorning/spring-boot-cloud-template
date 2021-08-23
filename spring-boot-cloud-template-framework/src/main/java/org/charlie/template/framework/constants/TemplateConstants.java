fe226f86b2098dceb3dbcc52c0a7a07000f8b13f
charlie <zhangchen143@gmail.com>
2021-08-23 19:58:14 +0800

    optimize comments

--- spring-boot-cloud-template-framework/src/main/java/org/charlie/template/framework/constants/TemplateConstants.java
+++ spring-boot-cloud-template-framework/src/main/java/org/charlie/template/framework/constants/TemplateConstants.java
@@ -6,6 +6,6 @@ package org.charlie.template.framework.constants;
  * @author Charlie
  */
 public class TemplateConstants {
-    public final static String TRACE_ID_HEADER_KEY = "global_trace_id";
-    public final static String TRACE_ID_MDC_KEY = "globalTraceId";
+    public final static String TRACE_ID_HEADER_KEY = "global_trace_id"; // used for fetching trace id in request header.
+    public final static String TRACE_ID_MDC_KEY = "globalTraceId"; // used for writing trace id in logs.
 }
