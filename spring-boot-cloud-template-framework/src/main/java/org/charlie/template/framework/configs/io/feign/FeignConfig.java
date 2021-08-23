12031bbd27368ac9eb041f4116b84fe5cc0f2b47
Charlie <zhangchen143@gmail.com>
2021-08-24 01:28:47 +0800

    switch http client to apache http client

--- spring-boot-cloud-template-framework/src/main/java/org/charlie/template/framework/configs/io/feign/FeignConfig.java
+++ spring-boot-cloud-template-framework/src/main/java/org/charlie/template/framework/configs/io/feign/FeignConfig.java
@@ -1,18 +1,32 @@
 package org.charlie.template.framework.configs.io.feign;
 
+import feign.Feign;
 import feign.Logger;
 import feign.RequestInterceptor;
 import feign.auth.BasicAuthRequestInterceptor;
 import feign.codec.ErrorDecoder;
+import feign.httpclient.ApacheHttpClient;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.charlie.template.framework.interceptors.out.feign.GlobalTraceIdRequestInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @Slf4j
+@ConditionalOnClass(Feign.class)
 @Configuration
 public class FeignConfig {
+
+    CloseableHttpClient closeableHttpClient;
+
+    @Autowired
+    public void setCloseableHttpClient(CloseableHttpClient closeableHttpClient) {
+        this.closeableHttpClient = closeableHttpClient;
+    }
 
     @Bean
     public Logger.Level Logger() {
@@ -32,5 +46,11 @@ public class FeignConfig {
     @Bean
     public ErrorDecoder errorDecoder() {
         return new CustomErrorDecoder();
+    }
+
+    @Bean
+    @ConditionalOnBean(CloseableHttpClient.class)
+    public ApacheHttpClient apacheHttpClient() {
+        return new ApacheHttpClient(closeableHttpClient);
     }
 }
