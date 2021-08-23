87043f63651bc3d2c7cea3eb566fba7a048cd5cc
charlie <zhangchen143@gmail.com>
2021-08-23 19:55:55 +0800

    optimize comments

--- spring-boot-cloud-template-framework/src/main/java/org/charlie/template/framework/utils/bean/SessionContext.java
+++ spring-boot-cloud-template-framework/src/main/java/org/charlie/template/framework/utils/bean/SessionContext.java
@@ -1,6 +1,13 @@
 package org.charlie.template.framework.utils.bean;
 
 
+/**
+ * Session Context to hold request information for each request.
+ * <p>
+ * Using InheritableThreadLocal for multi-thread occasion.
+ *
+ * @author Charlie
+ */
 public class SessionContext {
 
     private final static InheritableThreadLocal<Session> threadLocal = new InheritableThreadLocal<>();
