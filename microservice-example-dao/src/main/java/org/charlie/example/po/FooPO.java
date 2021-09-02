5199474c960456f02d307e048d2063e9aa2410b0
charlie <zhangchen143@gmail.com>
2021-09-02 21:06:05 +0800

    add jpa

--- microservice-example-dao/src/main/java/org/charlie/example/po/FooPO.java
+++ microservice-example-dao/src/main/java/org/charlie/example/po/FooPO.java
@@ -3,8 +3,11 @@ package org.charlie.example.po;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.persistence.Table;
+
 @Data
 @Builder
+@Table(name = "foo")
 public class FooPO {
     private int id;
     private String name;
